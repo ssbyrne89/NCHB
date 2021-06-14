ce370950c26b70d1896402cd484941ba424253c4
Sean Byrne <ssbyrne89@gmail.com>
2021-06-14 11:52:13 -0600

    added static pages

--- application.py
+++ application.py
@@ -5,3 +5,19 @@ application = Flask(__name__)
 @application.route("/")
 def index():
     return render_template("index.html")
+
+@application.route("/about")
+def about():
+    return render_template("about.html")
+
+@application.route("/wtmm")
+def wtmm():
+    return render_template("wtmm.html")
+
+@application.route("/real_estate_resources")
+def REresources():
+    return render_template("REresources.html")
+
+@application.route("/personal_finance_and_savings_resources")
+def PFSresources():
+    return render_template("PFSresources.html")
