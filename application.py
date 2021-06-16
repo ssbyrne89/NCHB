65acc8dccd95ef44cc2d24df0ad0b74ec3766581
Sean Byrne <ssbyrne89@gmail.com>
2021-06-15 23:04:59 -0600

    divided wtmm page into separate article pages

--- application.py
+++ application.py
@@ -26,3 +26,38 @@ def REresources():
 def PFSresources():
     return render_template("PFSresources.html")
 
+@application.route("/wtmm_sell_Ad_space_on_a_poster")
+def wtmm_sell_Ad_space_on_a_poster():
+    return render_template("wtmm_sell_Ad_space_on_a_poster.html")
+
+@application.route("/wtmm_start_a_youtube_channel")
+def wtmm_start_a_youtube_channel():
+    return render_template("wtmm_start_a_youtube_channel.html")
+
+@application.route("/wtmm_social_media_marketing")
+def wtmm_social_media_marketing():
+    return render_template("wtmm_social_media_marketing.html")
+    
+@application.route("/wtmm_cryptocurrency_interest_paying_accounts")
+def wtmm_cryptocurrency_interest_paying_accounts():
+    return render_template("wtmm_cryptocurrency_interest_paying_accounts.html")
+
+@application.route("/wtmm_designing_tshirts")
+def wtmm_designing_tshirts():
+    return render_template("wtmm_designing_tshirts.html")
+
+@application.route("/wtmm_bank_CD_accounts")
+def wtmm_bank_CD_accounts():
+    return render_template("wtmm_bank_CD_accounts.html")
+
+@application.route("/wtmm_running_a_node")
+def wtmm_running_a_node():
+    return render_template("wtmm_running_a_node.html")
+
+@application.route("/wtmm_browsing_the_internet")
+def wtmm_browsing_the_internet():
+    return render_template("wtmm_browsing_the_internet.html")
+
+@application.route("/wtmm_driving")
+def wtmm_driving():
+    return render_template("wtmm_driving.html")
